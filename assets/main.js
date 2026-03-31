cf8c1a6e53fc73c3baf0b8b74621f83a835f58fa
angello-maranholi <angellovmr@outlook.com>
2026-03-30 20:57:02 -0400

    NewWeb project with HTML/CSS/Js version 01

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+
+console.log("Js carregado com sucesso!");
+
+
+/*
+<script>
+        const botaoTeste = document.getElementById('botaoTeste');
+        botaoTeste.addEventListener('click', () => {
+            alert('Botão clicado!');
+        });
+    </script> */
