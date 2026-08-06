23fea6c6fc525199fc4abce4121929ad53e83ebc
angello-maranholi <angellovmr@outlook.com>
2026-08-06 07:07:39 -0400

    atualização 06/08/2026 parte 1

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -3,11 +3,9 @@ function pokeTypesToLi (types) {
     return types.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 }
 
-//APLICANDO A FORMA SIMPLIFICADA DA IMAGEM SEM PRECISAR CRIAR A FUNÇÃO 
+//FUNÇÃO PARA ADICIONAR A IMAGEM - Substituída por script direto no corpo do HTML 
 // function pokeImgToLi (pokemon){
-//     return `<img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">`
-// }
-
+//     return `<img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">`}
 // ${pokeImgToLi(pokemon)} - a ser colocado dentro do Li com a função pokeImgToLi
 
 function PokemonToLi(pokemon) {
@@ -29,7 +27,10 @@ function PokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-        
+        pokemonList.innerHTML += pokemons.map(PokemonToLi).join('');
+//função MAP super simplificada para substituir o FOR
+})
+
         // função FOR (loop)
         //const listItem = []
 
@@ -44,13 +45,10 @@ pokeApi.getPokemons().then((pokemons = []) => {
         //     return PokemonToLi(pokemon);
         // })
 
-        //simplificando o MAP
+        //função MAP simplificada
         // const listItem = pokemons.map(pokemon => PokemonToLi(pokemon))
         // console.log(listItem)
         // pokemonList.innerHTML += listItem;
 
-        //simplificando mais ainda o MAP - por padrão cada item percorrido pelo MAP é convertido diretamente em um item HTML da lista, sem precisar criar uma variável para adicionar. De forma mais simples ainda, o join ja executa a junção dos itens sem a vírgula de forma direta, eliminando a criação de outra variável, e tudo isso é passado diretamente para o innerHTMl, que tem a função de inserir o conteúdo manipulado aqui diretamente no corpo do HTML da página, através da ID do elemento, neste caso a pokemonList. 
-        pokemonList.innerHTML += pokemons.map(PokemonToLi).join('');
-       //Js Es6+ aplicado
-})
+        //**Função MAP super simplificada - por padrão cada item percorrido pelo MAP é convertido diretamente em um item HTML da lista, sem precisar criar uma variável para adicionar. De forma mais simples ainda, o join ja executa a junção dos itens sem a vírgula de forma direta, eliminando a criação de outra variável, e tudo isso é passado diretamente para o innerHTMl, que tem a função de inserir o conteúdo manipulado aqui diretamente no corpo do HTML da página, através da ID do elemento, neste caso a pokemonList. 
   
--- assets/Js/poke-api.js
+++ assets/Js/poke-api.js
@@ -16,22 +16,3 @@ pokeApi.getPokemons = (offset = 0, limit = 20) => {
             .then((pokeDetails) => pokeDetails);
 }
 
-// Promisse.all(
-//     for (let i = 0; i < 1154; i++) {
-//         const pokeId = pokeId[i];        
-//     }
-//     const pokeDetails = `https://pokeapi.co/api/v2/pokemon/${pokeId}`
-//     fetch(pokeDetails)
-// )
-
-// fetch(url)
-//versão mais curta:
-//     .then((response) => response.json())
-//     .then((jsonBody) => jsonBody.results)
-//     .then((pokemonList) => console.log(pokemonList))
-//versão mais longa:
-// .then(function (response) {
-//     response.json()
-//         .then(function(responseBody){
-//             console.log(responseBody);
-// })
